5d32513b352174c326ff012773ccff6dce0e9652
dimateniuta <dteniuta@gmail.com>
2022-11-30 11:12:33 +0300

    fix: corrected test for cube root

--- test/calculateCubeRoot.test.js
+++ test/calculateCubeRoot.test.js
@@ -4,9 +4,9 @@ import { it, expect, describe } from '@jest/globals';
 describe('calculateCubeRoot', () => {
   it('calculate', () => {
     expect(calculateCubeRoot(27)).toBe(3);
-    expect(calculateCubeRoot(-27)).toBe(3);
-    expect(calculateCubeRoot(125)).toEqual(4.999999999999999);
-    expect(calculateCubeRoot(-125)).toEqual(4.999999999999999);
+    expect(calculateCubeRoot(-27)).toBe(-3);
+    expect(calculateCubeRoot(125)).toEqual(5);
+    expect(calculateCubeRoot(-125)).toEqual(-5);
     expect(calculateCubeRoot(125)).not.toBe(0);
   });
 });
